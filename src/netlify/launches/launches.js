6daec55c21c7bf5965bee6dc3117fa66666c350a
Brady <brady.cunningham@ryerson.ca>
2021-04-09 00:30:44 -0400

    Add cors middleware to function

--- src/netlify/launches/launches.js
+++ src/netlify/launches/launches.js
@@ -16,6 +16,15 @@ const UpcomingLaunchesRoute = require("./upcomingLaunches");
 // Middleware
 app.use(express.json());
 app.use("/.netlify/functions/launches", UpcomingLaunchesRoute);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  next();
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
