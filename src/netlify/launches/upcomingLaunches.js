26b5837902c2f19e0aa57165632c3e8918f30b74
Brady <brady.cunningham@ryerson.ca>
2021-04-08 23:57:21 -0400

    Add async to function

--- src/netlify/launches/upcomingLaunches.js
+++ src/netlify/launches/upcomingLaunches.js
@@ -3,8 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 // Get all upcoming launches
-router.get("/", (req, res) => {
-  UpcomingLaunch.find({}, (err, results) => {
+router.get("/", async (req, res) => {
+  await UpcomingLaunch.find({}, (err, results) => {
     if (err) {
       res.status(400).json({
         message: "[ Error fetching upcomingLaunches ]: " + err,
